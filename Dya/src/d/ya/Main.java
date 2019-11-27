481b78f4be2b21dca289b1c419878639903ba285
Diagilev Nikolai <dyagilev_nikolaj@rambler.ru>
2019-11-27 13:18:45 +0300

    first commit

---
+++ Dya/src/d/ya/Main.java
@@ -0,0 +1,37 @@
+package d.ya;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        List<Integer> chanksOnServers = new ArrayList<>();
+
+        System.out.println("Введите 3 числа (кол-во чанков, серверов, запросы) :");
+        int chanks = in.nextInt();
+        int servers = in.nextInt();
+        int requests = in.nextInt();
+
+        System.out.println("расположение чанков на серверах :" );
+        for (int i = 0; i < chanks; i++) {
+            chanksOnServers.add(in.nextInt());
+        }
+        System.out.println(requests + " запросов: " );
+        for (int i = 0; i < requests; i++) {
+            List requestsList = new ArrayList();
+
+            int serverA = in.nextInt();
+            int serverB = in.nextInt();
+            int chankFrom = in.nextInt();
+            int chankTo = in.nextInt();
+
+            for (int j = chankTo; j < chankFrom; j++) {
+                System.out.println(chanksOnServers.get(j));
+            }
+
+        }
+    }
+}
