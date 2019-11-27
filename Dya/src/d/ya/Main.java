d96e6a343b0471a592289bfb730c934e49b4da85
Diagilev Nikolai <dyagilev_nikolaj@rambler.ru>
2019-11-27 14:10:22 +0300

    3 commit

--- Dya/src/d/ya/Main.java
+++ Dya/src/d/ya/Main.java
@@ -10,28 +10,36 @@ public class Main {
         Scanner in = new Scanner(System.in);
         List<Integer> chanksOnServers = new ArrayList<>();
 
-        System.out.println("Введите 3 числа (кол-во чанков, серверов, запросы) :");
+//        System.out.println("Введите 3 числа (кол-во чанков, серверов, запросы) :");
         int chanks = in.nextInt();
-//        int servers = in.nextInt();
+        int servers = in.nextInt();
         int requests = in.nextInt();
 
-        System.out.println("расположение чанков на серверах :" );
+//        System.out.println("расположение чанков на серверах :" );
         for (int i = 0; i < chanks; i++) {
             chanksOnServers.add(in.nextInt());
         }
-        System.out.println(requests + " запросов: " );
+//        System.out.println(requests + " запросов: " );
         for (int i = 0; i < requests; i++) {
-            List requestsList = new ArrayList();
-
             int serverA = in.nextInt();
             int serverB = in.nextInt();
             int chankFrom = in.nextInt();
             int chankTo = in.nextInt();
 
-            for (int j = chankTo; j < chankFrom; j++) {
-                System.out.println(chanksOnServers.get(j));
+            int amountOfTries = 0;
+            int amountOfYes = 0;
+            for (int j = chankFrom-1; j <= chankTo - 1; j++) {
+                if(chanksOnServers.get(j)==serverA){
+                    amountOfYes = amountOfYes + 1;
+                }
+                amountOfTries++;
             }
 
+            if(amountOfTries==amountOfYes && serverB<=servers){
+                System.out.println(1);
+            } else {
+                System.out.println(0);
+            }
         }
     }
 }
